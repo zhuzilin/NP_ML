d4a71dbd51e687e7278d0981277ce95986de042a
zhuzilin <zhuzl14@mails.tsinghua.edu.cn>
2018-04-27 17:31:17 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '0.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='NP_ML',
+    version=__version__,
+    description='A tool library of classical machine learning algorithms with only numpy.',
+    url='https://github.com/zhuzilin/NP_ML',
+    download_url='https://github.com/zhuzilin/NP_ML',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    author='zhuzilin',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='zhuzilinallen@gmail.com'
+)
